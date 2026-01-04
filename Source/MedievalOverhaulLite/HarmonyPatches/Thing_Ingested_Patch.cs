1c4b5355ae0ae7aae870390896580e0f9ce154de
YardenDrori <yarden.drori.yd@gmail.com>
2026-01-05 01:56:24 +0200

    made pawns spawn dirty bowl after eating

---
+++ Source/MedievalOverhaulLite/HarmonyPatches/Thing_Ingested_Patch.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace MOExpandedLite
+{
+  [HarmonyPatch(typeof(Thing), "PostIngested")]
+  public static class PostIngested_MakeBowl_Patch
+  {
+    //caching cause im cool like that tho idk if this is an expensive operation i assume it is as it invlovesa a string
+    private static ThingDef bowlDef = DefDatabase<ThingDef>.GetNamed("MOL_PlateDirty");
+
+    [HarmonyPostfix]
+    public static void Postfix(Thing __instance, Pawn ingester)
+    {
+      if (
+        __instance.def.ingestible.foodType.HasFlag(FoodTypeFlags.Meal)
+        && __instance.def.ingestible.tableDesired
+      )
+      {
+        Thing bowl = ThingMaker.MakeThing(bowlDef, null);
+        bowl.stackCount = 1;
+        GenPlace.TryPlaceThing(bowl, ingester.Position, ingester.Map, ThingPlaceMode.Direct);
+
+        Verse.AI.Job haulJob = Verse.AI.HaulAIUtility.HaulToStorageJob(ingester, bowl, true);
+
+        haulJob.count = bowl.stackCount;
+        haulJob.targetA = bowl;
+
+        if (haulJob != null)
+        {
+          ingester.jobs.jobQueue.EnqueueFirst(haulJob);
+        }
+      }
+    }
+  }
+}
